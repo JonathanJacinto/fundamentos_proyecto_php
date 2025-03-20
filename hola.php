3dc95f7c36a70b556f3d41db1b5e58c643eeb9fc
Jonathan Jacinto <24011285@galileo.edu>
2025-03-20 01:19:53 +0000

    initial commit

---
+++ hola.php
@@ -0,0 +1,12 @@
+<?php
+
+    $variable_nombre = "Jonathan Jacinto";
+
+    echo "Hola mundo, soy " . $variable_nombre;
+    echo "<br>";
+
+
+
+?>
+
+<img src="/Actividad3/proyecto_php/gato.jpg" alt="Gato ingeniero">
